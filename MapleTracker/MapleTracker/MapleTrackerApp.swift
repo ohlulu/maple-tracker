768e0951e592e65cba908a541d717188ba6d7e4e
Ohlulu Hsih <z30262226@gmail.com>
2025-06-23 12:32:03 +0800

    init project

---
+++ MapleTracker/MapleTracker/MapleTrackerApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2025 Ohlulu. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MapleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapleTracker/MapleTrackerTests/MapleTrackerTests.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2025 Ohlulu. All rights reserved.
+//
+
+import Testing
+@testable import MapleTracker
+
+struct MapleTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
