ef1f2b0b65b8645cc6609bccd60f54d37a657e39
Ohlulu Hsih <z30262226@gmail.com>
2025-06-27 11:38:47 +0800

    refactor: clean up AppColors by removing unused color definitions

--- MapleTracker/MapleTracker/Colors.swift
+++ MapleTracker/MapleTracker/Colors.swift
@@ -4,7 +4,6 @@ enum AppColors {
     // Background colors
     static let primaryBackground = Color(red: 0.10, green: 0.10, blue: 0.10) // #1a1a1a
     static let containerBackground = Color(red: 0.15, green: 0.15, blue: 0.15) // #262626
-    static let headerBackground = Color(red: 0.20, green: 0.20, blue: 0.20) // #333
     static let cardBackground = Color(red: 0.16, green: 0.16, blue: 0.16) // #2a2a2a
     static let listBackground = Color(red: 0.10, green: 0.10, blue: 0.10) // #1a1a1a
     static let itemBackground = Color(red: 0.13, green: 0.13, blue: 0.13) // #222
@@ -19,7 +18,6 @@ enum AppColors {
     static let primaryText = Color(red: 0.79, green: 0.82, blue: 0.85) // #c9d1d9
     static let secondaryText = Color(red: 0.90, green: 0.90, blue: 0.90) // #e6e6e6
     static let tertiaryText = Color(red: 0.63, green: 0.63, blue: 0.63) // #a0a0a0
-    static let mutedText = Color(red: 0.60, green: 0.60, blue: 0.60) // #999
     static let subtleText = Color(red: 0.53, green: 0.53, blue: 0.53) // #888
     static let expText = Color(red: 0.67, green: 0.67, blue: 0.67) // #aaa
     static let indicatorText = Color(red: 0.73, green: 0.73, blue: 0.73) // #bbb
@@ -27,10 +25,8 @@ enum AppColors {
     
     // Button colors
     static let buttonBackground = Color(red: 0.40, green: 0.40, blue: 0.40) // #666
-    static let buttonHover = Color(red: 0.47, green: 0.47, blue: 0.47) // #777
     static let buttonText = Color.white
     
     // Accent colors
     static let pulseColor = Color(red: 0.60, green: 0.60, blue: 0.60) // #999
-    static let tallyColor = Color(red: 0.27, green: 0.27, blue: 0.27) // #444
 }
